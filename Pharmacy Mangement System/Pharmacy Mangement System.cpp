edf18e16ddf92171fd90b65df4e55b30033c48f1
Peter Emad <139802433+Peter-Emad100@users.noreply.github.com>
2024-03-20 14:49:37 +0200

    search for medicine by category and intializer
    implemented an initializer to initialize data that can be used for test purpose for other function and implemented search for medicine by category

--- Pharmacy Mangement System/Pharmacy Mangement System.cpp
+++ Pharmacy Mangement System/Pharmacy Mangement System.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
+const int Size=100;
 struct medicine {
 	int ID;
 	string name;
@@ -8,8 +9,19 @@ struct medicine {
 	string category;
 	float price;
 	int quantity_in_stock;
+	void initialize(int _id, string _name, string _desc, bool _avail, string _category, float _price, int _quantity) {
+		ID = _id;
+		name = _name;
+		description = _desc;
+		availability = _avail;
+		category = _category;
+		price = _price;
+		quantity_in_stock = _quantity;
+	}
 };
-medicine medicines[100];
+medicine medicines[Size];
+
+	;
 struct user {
 	int ID;
 	string username;
@@ -19,7 +31,7 @@ struct user {
 	string phone;
 	enum role{ User, Admin };
 };
-user users[100];
+user users[Size];
 struct order {
 	int userID;
 	string orderDate;
@@ -27,8 +39,31 @@ struct order {
 	float totalPrice;
 	string shipDate;
 };
-order orders[100];
+order orders[Size];
+void searchForMedicineByCategory() {
+	string category;
+	cin >> category;
+	bool found = false;
+	for (int i = 0; i < Size; i++) {
+		if (category == medicines[i].category) {
+			cout << medicines[i].name << " -- " << medicines[i].ID << " -- " << medicines[i].category << endl;
+			found = true;
+		}
+	}
+	if (found == false) {
+		cout << "there is no medicine meet this category" << endl;
+	}
+}
 int main()
 {
-
+	medicines[0].initialize(1, "Paracetamol", "Pain reliever and fever reducer", true, "Analgesic",5.99, 100);
+	medicines[1].initialize(2, "Lisinopril", "Used to treat high blood pressure", true, "Antihypertensive", 10.49, 50);
+	medicines[2].initialize(3, "Omeprazole", "Treats heartburn, stomach ulcers, and gastroesophageal reflux disease (GERD)", true, "Gastrointestinal", 7.25, 80);
+	medicines[3].initialize(4, "Atorvastatin", "Lowers high cholesterol and triglycerides", true, "Lipid-lowering agent", 15.75, 30);
+	medicines[4].initialize(5, "Metformin", "Treats type 2 diabetes", true, "Antidiabetic", 8.99, 60);
+	medicines[5].initialize(6, "Amoxicillin", "Antibiotic used to treat bacterial infections", true, "Antibiotic", 6.50, 120);
+	medicines[6].initialize(7, "Alprazolam", "Treats anxiety and panic disorders", true, "Anxiolytic", 12.99, 40);
+	medicines[7].initialize(8, "Ibuprofen", "Nonsteroidal anti-inflammatory drug (NSAID)", true, "Analgesic", 4.75, 200);
+	medicines[8].initialize(9, "Cetirizine", "Antihistamine used for allergy relief", true, "Antihistamine", 9.25, 70);
+	medicines[9].initialize(10, "Ranitidine", "Reduces stomach acid production to treat heartburn and ulcers", true, "Gastrointestinal", 6.99, 90);
 }
